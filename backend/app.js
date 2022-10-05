a7cd3c2bd21057f9362be3d69168c186ad9e9fd5
MJigah <jigah4july@gmailcom>
2022-10-05 02:49:41 +0100

    updated modules

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,12 @@
 const { application } = require('express');
 const express = require('express');
-const app =
+const app = express()
+const port = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+})
+
+app.listen(port, () => {
+    console.log(`This server is running on port ${port}`)
+})
